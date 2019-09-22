62f91bebe16b4afa828c5fbc2db3e89eed0d2071
Freddy Munive <munivef@yahoo.com>
2019-09-22 15:30:00 -0500

    creando bd en firebase, subiendo img al storage y mostrando info en html

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,17 +8,30 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
     // console.log('Servicio de pagina cargada');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     //leer el archivo JSON
     this.http
       .get('assets/data/data-page.json')
       .subscribe((resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        // console.log(resp);
+      });
+  }
+
+  private cargarEquipo() {
+    this.http
+      .get('https://angular-html-44e88.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+        this.equipo = resp;
+        console.log(resp);
       });
   }
 }
