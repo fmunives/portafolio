7fb85f394a25f27b6a2ed39fdf7b846d763a300c
Freddy Munive <munivef@yahoo.com>
2019-09-22 14:45:39 -0500

    finalizando rutas jSON y animaciones

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,12 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
   anio: number = new Date().getFullYear();
 
-  constructor() { }
+  constructor(public _servicio: InfoPaginaService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
